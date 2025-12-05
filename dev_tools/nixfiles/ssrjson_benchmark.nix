4e0ccfb80ee03db0bc128841052d5db231ce196b
antares <antares@alyr.dev>
2025-12-06 00:44:30 +0800

    Update ssrjson benchmark version

--- dev_tools/nixfiles/ssrjson_benchmark.nix
+++ dev_tools/nixfiles/ssrjson_benchmark.nix
@@ -16,22 +16,22 @@ let
 in
 pypkgs.buildPythonPackage rec {
   pname = "ssrjson_benchmark";
-  version = "0.0.7";
+  version = "0.0.9";
   pyproject = true;
 
   disabled = pypkgs.pythonOlder "3.10";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "Nambers";
-    repo = "ssrJSON-benchmark";
-    rev = "bac02bd715cb78d54ec4ec298246abb576e53e5e";
-    sha256 = "sha256-XQaiHDuqrcI3JNpjvL9emzaPM+qHuXNrJOG4/5eFJj8=";
-  };
-
-  # src = fetchPypi {
-  #   inherit pname version;
-  #   sha256 = "sha256-C3Emj2fXSZmoV34rqf+fHy1M+Ta00dMRpCX9QOeshqk=";
+  # src = pkgs.fetchFromGitHub {
+  #   owner = "Nambers";
+  #   repo = "ssrJSON-benchmark";
+  #   rev = "0cab0745e486b7f61559d50ea2dada34a477cc2e";
+  #   sha256 = "sha256-wmiSxAlPMHgtnUnr0RlyXVSQaRxfmtVPh8cU0vmQtgw=";
   # };
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-tcmS0HLaKIECxD9QftdzfEdFd8hKzKozZGQh5z1oU1Y=";
+  };
 
   build-system = with pypkgs; [ setuptools ];
 
