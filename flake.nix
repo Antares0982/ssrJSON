19765e32d037170c91743776df5e9644457b859c
antares <antares@alyr.dev>
2026-01-11 01:36:31 +0800

    Move nix shell to ssrjson-nix-dev

--- flake.nix
+++ flake.nix
@@ -2,18 +2,18 @@
   description = "ssrjson flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-legacy.url = "github:NixOS/nixpkgs/nixos-25.05";
+    ssrjson-nix-dev.url = "github:Antares0982/ssrjson-nix-dev/main";
   };
 
   outputs =
     inputs@{
       self,
-      nixpkgs,
-      nixpkgs-legacy,
+      ssrjson-nix-dev,
       ...
     }:
     let
+      nixpkgs = ssrjson-nix-dev.ssrjson-nixpkgs;
+      nixpkgs-legacy = ssrjson-nix-dev.ssrjson-nixpkgs-legacy;
       forAllSystems =
         function:
         nixpkgs.lib.genAttrs
@@ -30,72 +30,9 @@
               }
             )
           );
-
     in
     {
-      devShells = forAllSystems (
-        pkgs:
-        let
-          pkgs-legacy = import nixpkgs-legacy { inherit (pkgs.stdenv.hostPlatform) system; };
-          versionUtils = pkgs.callPackage ./dev_tools/nixfiles/version_utils.nix { inherit pkgs-legacy; };
-          defaultShell = pkgs.callPackage ./dev_tools/nixfiles/shell.nix {
-            inherit pkgs-legacy;
-          };
-          _drvs = pkgs.callPackage ./dev_tools/nixfiles/_drvs.nix { inherit pkgs-legacy; };
-          pythonVerConfig = versionUtils.pythonVerConfig;
-          curVer = pythonVerConfig.curVer;
-          leastVer = pythonVerConfig.minSupportVer;
-          verLength = curVer - leastVer;
-          mkMyShell =
-            { shell, ... }:
-            (
-              (shell.overrideAttrs {
-                shellHook = pkgs.callPackage ./dev_tools/nixfiles/shellhook.nix {
-                  parentShell = shell;
-                  inherit pkgs-legacy;
-                  inherit (shell) inputDerivation;
-                  inherit (_drvs) pyenvs debuggable_py pyenv_nodebug;
-                  nix_pyenv_directory = ".nix-devenv";
-                  pyenv = builtins.elemAt _drvs.pyenvs verLength;
-                  using_python = builtins.elemAt _drvs.using_pythons verLength;
-                };
-              })
-              // {
-                super = shell;
-              }
-            );
-          verToBuildEnvDef = ver: {
-            name = "buildenv-py3" + (toString ver);
-            value = pkgs.mkShell {
-              buildInputs = [
-                (
-                  (builtins.getAttr ("python3" + (toString ver)) (if ver >= 10 then pkgs else pkgs-legacy))
-                  .withPackages
-                  (
-                    pypkgs: with pypkgs; [
-                      # this is needed unless `nix build nixpkgs#python314Packages.pip` can run correctly
-                      (if ver < 14 then pip else pkgs.callPackage ./dev_tools/nixfiles/py314-pip.nix { inherit pypkgs; })
-                      build
-                      pytest
-                      pytest-random-order
-                    ]
-                  )
-                )
-              ]
-              ++ (with pkgs; [
-                cmake
-                clang
-              ]);
-              hardeningDisable = [ "fortify" ];
-            };
-          };
-        in
-        {
-          internal = defaultShell;
-          default = mkMyShell { shell = defaultShell; };
-        }
-        // (builtins.listToAttrs (map verToBuildEnvDef versionUtils.versions))
-      );
+      inherit (ssrjson-nix-dev) devShells;
       packages = forAllSystems (
         pkgs:
         let
